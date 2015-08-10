3a0ffd88e3c03e9fd78904dce9f000fbb1bbd17b
xubo2004 <xubo2004@gmail.com>
2015-08-10 23:33:17 +0800

    add zeromq demo.
    http://www.codeproject.com/Articles/488207/ZeroMQ-via-Csharp-Introduction

---
+++ zookeeperDemo/zookeeperDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Org.Apache.Zookeeper.Data;
+using ZooKeeperNet;
+
+namespace zookeeperDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoWatcher watcher = new DemoWatcher();
+            ZooKeeper zk = new ZooKeeper("192.168.6.129:2182", new TimeSpan(16000), watcher);
+            //zk.Create("/master", BitConverter.GetBytes(230), ACL, CreateMode.Ephemeral);
+            checkMaster(zk);
+
+            Console.ReadLine();
+        }
+
+        private static bool checkMaster(ZooKeeper zk)
+        {
+            while (true)
+            {
+                try
+                {
+                    Stat stat = new Stat();
+                    byte[] data = zk.GetData("/master", false, stat);
+                    var isLeader = System.Text.Encoding.UTF8.GetString(data).Equals("230");
+                    return true;
+                }
+                catch (KeeperException.NoNodeException)
+                {
+                    
+                    return false;
+                }
+            }
+        }
+    }
+
+    public class DemoWatcher : IWatcher
+    {
+        public void Process(WatchedEvent @event)
+        {
+            Console.WriteLine(@event);
+
+        }
+    }
+}
